d7c6b5117b732b138387ca9ad449ead0808d8511
tomasprison060 <tomasprison060@gmail.com>
2026-01-18 05:55:35 +0200

    Add files via upload

---
+++ load.lua
@@ -0,0 +1,11 @@
+local StarterGui = game:GetService("StarterGui")
+if game.PlaceId == 2413927524 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/tomasprison060/dolarovhub/refs/heads/main/Dolarov%20hub.lua"))()
+else
+    StarterGui:SetCore("SendNotification", {
+         Title = "this game is not supported!"
+         Text = "place id invalid!",
+         Duration = 5,
+         Button1 = "OK",
+    })
+end
